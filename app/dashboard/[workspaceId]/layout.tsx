0879bce23dc7d9ed4793fac142bb3f99f7045124
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-11-06 13:32:09 +0530

    auth using workspace id

--- app/dashboard/[workspaceId]/layout.tsx
+++ app/dashboard/[workspaceId]/layout.tsx
@@ -1,14 +1,18 @@
- import React from 'react'
- 
- type Props = {
-    params:{workspaceId:string}
-    children:React.ReactNode
- }
- 
- const Layout = ({params:{workspaceId},children}:Props) => {
-   return (
-     <div>Layout</div>
-   )
- }
+import { onAuthenticateUser } from "@/actions/user";
+import { redirect } from "next/navigation";
+import React from "react";
 
- export default Layout
+type Props = {
+  params: { workspaceId: string };
+  children: React.ReactNode;
+};
+
+const Layout = async ({ params: { workspaceId }, children }: Props) => {
+  const auth = await onAuthenticateUser();
+  if (!auth.user?.workspace) redirect("/auth/sign-in");
+  if (!auth.user?.workspace.length) redirect("/auth/sign-in");
+  const  hasAccess = await verifyAccessToWorkspace(workspaceId);
+  return <div>Layout</div>;
+};
+
+export default Layout;
